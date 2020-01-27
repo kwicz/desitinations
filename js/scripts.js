674afa8d43cd9c78501c5f9e421175cd62de7323
kwicz and Matt Taylor <matt@>
2020-01-27 14:00:20 -0800

    add user output

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,13 @@ function DestinationList() {
 };
 
 DestinationList.prototype.addDest = function(destination) {
-  destination.id = this.assignId();
+  destination.id = this.currentId;
+  console.log("dest id: " + destination.id);
   this.destinations.push(destination);
+  this.currentId++;
+  console.log("dest id ++: " + this.currentId);
 };
 
-DestinationList.prototype.assignId = function() {
-  this.currentId++;
-  return this.currentId;
-};
 
 function destination(location, landmarks, timeOfYear, activities, cost) {
   this.location = location;
@@ -31,24 +30,18 @@ $(document).ready(function() {
   var destinationList = new DestinationList();
 
   $("#output").on("click","ul",function() {
-    var id = event.target.id;
-    var test = destinationList.destinations[0].location;
-    console.log(test)
+    var id = parseInt(event.target.id);
+    console.log("id: " + id)
+    var userDestination = destinationList.destinations[id];
+    console.log("test: " + destinationList.destinations[id]);
 
-    // var id = $("ul#id").val();
-    // console.log(id)
-    // $(this).append("<li>" + this.landmarks + "</li> <li>" + this.timeOfYear + "</li> <li>" + this.activities + "</li> <li>" + this.cost + "</li>")
+    $(this).append("<li>" + userDestination.landmarks + "</li> <li>" + userDestination.timeOfYear + "</li> <li>" + userDestination.activities + "</li> <li>" + userDestination.cost + "</li>")
   })
 
   $("form").submit(function(event) {
     event.preventDefault();
-    
     var dest = new destination($("#location").val(),$("#landmarks").val(),$("#timeOfYear").val(),$("#activities").val(),$("#cost").val());
-    console.log(dest)
     destinationList.addDest(dest);
     $("#output").append("<ul id='" + dest.id + "'>" + dest.location + "</ul>");
-
-    console.log(destinationList)
-    console.log(dest.id);
   }) 
 })
